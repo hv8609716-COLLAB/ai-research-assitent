4e4db9dd7370a7567a0f7d729d5aa271f83ef4ec
HARSH VARDHAN <hv8609716@gmail.com>
2026-07-05 11:16:00 +0530

    Refactor Flask app and update home route response
    Refactored the Flask app to simplify the import statements and modified the home route to return HTML directly instead of rendering a template.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, request
 import os
 from pdf_reader import read_pdf
 from qa_engine import answer_question
 
-app = Flask(__name__, template_folder='templates')
-
+app = Flask(__name__)
 document_text = ""
 
 @app.route("/", methods=["GET", "POST"])
@@ -22,10 +21,23 @@ def home():
         question = request.form.get("question", "")
         if question:
             if document_text:
-                response = answer_question(question, document_text)
+                response = answer_question(
+                    question, document_text)
             else:
                 response = "Pehle PDF upload karo"
-    return render_template("index.html", response=response)
+    return (
+        "<h1>AI Research Assistant</h1>"
+        "<h3>by Harsh Vardhan</h3>"
+        "<form method='POST' "
+        "enctype='multipart/form-data'>"
+        "<input type='file' name='pdf'><br><br>"
+        "<input type='text' name='question' "
+        "placeholder='Question...' size='40'>"
+        "<br><br>"
+        "<input type='submit' value='Submit'>"
+        "</form>"
+        f"<p>{response}</p>"
+    )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=10000)
